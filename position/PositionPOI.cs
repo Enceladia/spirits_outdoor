20ccb40c8da05ae006a91f2c0b12afc9a9e5e7b6
christina.pagger <christina.appdev@gmail.com>
2017-04-06 12:08:47 +0200

    Added XML Places Parsing

---
+++ position/PositionPOI.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Dummi Klasse für generische Punkte
+public class PositionPOI {
+    public string poi_name;
+    public string poi_place;
+    public List<String> poi_typeList = new List<String>();
+    public float poi_lat;
+    public float poi_lng;
+    public string poi_iconUrl;
+    public string poi_id;
+}
---
+++ position/PositionPlaceParser.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using UnityEngine;
+
+public class PositionPlaceParser : MonoBehaviour {
+
+    public List<PositionPOI> poi_list = new List<PositionPOI>();
+
+	// Use this for initialization
+	void Start () {
+        TextAsset textAsset = (TextAsset)Resources.Load("xml");
+
+        parsePlacesXML(textAsset.text);
+    }
+	
+    public void parsePlacesXML(string placesXML)
+    {
+        XmlDocument xmlDoc = new XmlDocument();
+        xmlDoc.LoadXml(placesXML);
+
+        XmlNodeList nodeList = xmlDoc.GetElementsByTagName("result");
+
+        for (int i = 0; i < nodeList.Count; i++)
+        {
+            poi_list.Add(new PositionPOI());
+
+            foreach (XmlNode node in nodeList[i])
+            {
+                
+
+                if (node.Name == "name")
+                {
+                    poi_list[i].poi_name = node.InnerText;
+                }
+
+                if (node.Name == "vicinity")
+                {
+                    poi_list[i].poi_place = node.InnerText;
+
+                }
+
+                if (node.Name == "geometry")
+                {
+                    foreach(XmlNode geometry in node)
+                    {
+                        if (geometry.Name == "location")
+                        {
+                            foreach (XmlNode location in geometry)
+                            {
+                                if (location.Name == "lat")
+                                {
+                                    poi_list[i].poi_lat = float.Parse(location.InnerText);
+
+                                }
+                                if (location.Name == "lng")
+                                {
+                                    poi_list[i].poi_lng = float.Parse(location.InnerText);
+                                }
+                            }
+                        }           
+                    }
+                }
+
+                if (node.Name == "icon")
+                {
+                    poi_list[i].poi_iconUrl = node.InnerText;
+                }
+
+                if (node.Name == "place_id")
+                {
+                    poi_list[i].poi_id = node.InnerText;
+                }
+
+                if (node.Name == "type")
+                {
+                    poi_list[i].poi_typeList.Add(node.InnerText);
+                }
+            }
+        }
+
+
+        foreach (PositionPOI poiObj in poi_list)
+        {
+            Debug.Log(poiObj.poi_name);
+            Debug.Log(poiObj.poi_place);
+
+            Debug.Log(poiObj.poi_lat);
+            Debug.Log(poiObj.poi_lng);
+
+            foreach (string s in poiObj.poi_typeList)
+            {
+                Debug.Log(s);
+            }
+
+            Debug.Log(poiObj.poi_iconUrl);
+            Debug.Log(poiObj.poi_id);
+
+            Debug.Log("---------------------------------------------------------------------");
+        }
+    }
+}
